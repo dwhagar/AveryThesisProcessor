a7bad8f927bd3ff23931da3d6b46ff7bed13380d
dwhagar <david.hagar@gmail.com>
2019-02-09 20:36:17 -0500

    Added options to process one file or all files in a directory or entire directory tree.

--- xml-process.py
+++ xml-process.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os.path
+from os import walk
 import xml.etree.ElementTree as ET
 
 # Import custom class.
@@ -12,106 +13,137 @@ def urlScrub(data):
     result = data.split("}")[1]
     return result
 
+def findXML(dir, r = False):
+    """Takes a directory specification and produces a list of XML files."""
+    result = []
+
+    for (dirpath, dirnames, filenames) in walk(dir):
+        for file in filenames:
+            if file[-3:].lower() == "xml":
+                result.append(os.path.join(dirpath, file))
+
+        if not r:
+            dirnames.clear()
+
+    return result
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", type=str, help="The name of the XML file to be processed.", required=True)
-
-    # TODO: Add ability to scan all file sin a directory.
-    # TODO: Add option for output to a CSV file
+    parser.add_argument("-f", "--file", type=str, help="The name of the XML file to be processed.")
+    parser.add_argument("-d", "--dir", type=str, help="The directory name to find XML files to processed.")
+    parser.add_argument("-o", "--output-child", type=str, help="The name of the CSV file to output the child data.")
+    parser.add_argument("-a", "--output-adult", type=str, help="The name of the CSV file to output the adult data.")
+    parser.add_argument("-r", "--recursive", type=bool, help="Should a directory be looked at recursively.")
 
     arg = parser.parse_args()
 
-    if not os.path.isfile(arg.file):
-        print("File" + arg.file + "not found.")
+    if arg.file == "" and arg.dir == "":
+        print("You must specify either a file or directory to process.")
         return 1
 
-    print("Processing file " + arg.file + "...")
+    fileList = []
 
-    corpusTree = ET.parse(arg.file)
-    corpusRoot = corpusTree.getroot()
+    # Process any file and directory names and make a list to iterate through.
+    if not arg.file == "":
+        if not os.path.isfile(arg.file):
+            print("File " + arg.file + " not found or is not a file.")
+            return 1
+        fileList.append(arg.file)
+    elif not arg.dir == "":
+        if not os.path.isdir(arg.dir):
+            print("Directory " + arg.dir + " not found or is not a directory.")
+            return 1
+        fileList = findXML(dir, arg.recursive)
 
-    allParts = []
+    # Process all the XML files in the list.
+    for file in fileList:
+        print("Processing file " + file + "...")
 
-    # Process from the root down.
-    for chld in corpusRoot:
-        # Process the Participants
-        if urlScrub(chld.tag) == "participants":
-            role = None
-            name = None
-            sex = None
-            age = None
-            lang = None
+        corpusTree = ET.parse(arg.file)
+        corpusRoot = corpusTree.getroot()
 
-            for part in chld:
-                # Gather every participant in turn.
-                if urlScrub(part.tag) == "participant":
-                    sid = part.attrib['id']
+        allParts = []
 
-                    # Gather the actual data.
-                    for partData in part:
-                        if urlScrub(partData.tag) == "role":
-                            role = partData.text
-                        elif urlScrub(partData.tag) == "name":
-                            name = partData.text
-                        elif urlScrub(partData.tag) == "sex":
-                            sex = partData.text
-                        elif urlScrub(partData.tag) == "age":
-                            age = partData.text
-                        elif urlScrub(partData.tag) == "language":
-                            lang = partData.text
+        # Process from the root down.
+        for chld in corpusRoot:
+            # Process the Participants
+            if urlScrub(chld.tag) == "participants":
+                role = None
+                name = None
+                sex = None
+                age = None
+                lang = None
 
-                    curPart = speaker.Speaker(sid, role, name, sex, age, lang)
-                    allParts.append(curPart)
+                for part in chld:
+                    # Gather every participant in turn.
+                    if urlScrub(part.tag) == "participant":
+                        sid = part.attrib['id']
 
-        # Process the actual word data, starting with the speaker.
-        elif urlScrub(chld.tag) == "transcript":
-            for u in chld:
-                # Provide a place to store a reference to the current speaker information.
-                s = None
-                # Gather the speaker from the <u> tag.
-                if urlScrub(u.tag) == "u":
-                    sid = u.attrib['speaker']
+                        # Gather the actual data.
+                        for partData in part:
+                            if urlScrub(partData.tag) == "role":
+                                role = partData.text
+                            elif urlScrub(partData.tag) == "name":
+                                name = partData.text
+                            elif urlScrub(partData.tag) == "sex":
+                                sex = partData.text
+                            elif urlScrub(partData.tag) == "age":
+                                age = partData.text
+                            elif urlScrub(partData.tag) == "language":
+                                lang = partData.text
 
-                    # Now find the speaker by the ID in the list.
-                    for part in allParts:
-                        if part.sid == sid:
-                            # When the speaker ID is found, make a reference to it.
-                            s = part
+                        curPart = speaker.Speaker(sid, role, name, sex, age, lang)
+                        allParts.append(curPart)
 
-                    # Now process the text via the groupTier tag under Morphology
-                    for g in u:
-                        if urlScrub(g.tag) == "groupTier":
-                            if g.attrib['tierName'] == "Morphology":
-                                # Record if a noun is seen before an adjective.
-                                seenNoun = False
+            # Process the actual word data, starting with the speaker.
+            elif urlScrub(chld.tag) == "transcript":
+                for u in chld:
+                    # Provide a place to store a reference to the current speaker information.
+                    s = None
+                    # Gather the speaker from the <u> tag.
+                    if urlScrub(u.tag) == "u":
+                        sid = u.attrib['speaker']
 
-                                # Each word is stored in a <tg> tag and under that a <w> tag.
-                                for t in g:
-                                    if urlScrub(t.tag) == "tg":
-                                        for w in t:
-                                            if urlScrub(w.tag) == "w":
-                                                if not (w.text[0] == "(" or w.text[-1] == ")"):
-                                                    word = speaker.Word(w.text)
-                                                    if word.noun:
-                                                        seenNoun = True
-                                                    elif word.adj:
-                                                        word.adj = True
-                                                        if seenNoun:
-                                                            word.beforeNoun = False
-                                                        else:
-                                                            word.beforeNoun = True
+                        # Now find the speaker by the ID in the list.
+                        for part in allParts:
+                            if part.sid == sid:
+                                # When the speaker ID is found, make a reference to it.
+                                s = part
 
-                                                    # Store the word into the list for this speaker.
-                                                    if not word.punctuation:
-                                                        s.words.append(word)
+                        # Now process the text via the groupTier tag under Morphology
+                        for g in u:
+                            if urlScrub(g.tag) == "groupTier":
+                                if g.attrib['tierName'] == "Morphology":
+                                    # Record if a noun is seen before an adjective.
+                                    seenNoun = False
 
-        for part in allParts:
-            print(part.sid)
-            for word in part.words:
-                print(word.word)
+                                    # Each word is stored in a <tg> tag and under that a <w> tag.
+                                    for t in g:
+                                        if urlScrub(t.tag) == "tg":
+                                            for w in t:
+                                                if urlScrub(w.tag) == "w":
+                                                    if not (w.text[0] == "(" or w.text[-1] == ")"):
+                                                        word = speaker.Word(w.text)
+                                                        if word.noun:
+                                                            seenNoun = True
+                                                        elif word.adj:
+                                                            word.adj = True
+                                                            if seenNoun:
+                                                                word.beforeNoun = False
+                                                            else:
+                                                                word.beforeNoun = True
+
+                                                        # Store the word into the list for this speaker.
+                                                        if not word.punctuation:
+                                                            s.words.append(word)
+
+    if len(allParts) < 1:
+        print("There was an error processing the data, no participants were found.")
+        return 1
+
+    for spk in allParts:
+        continue
 
     return 0
 
 main()
-
-# TODO:  Remove all text after the ampersand in the data file.
