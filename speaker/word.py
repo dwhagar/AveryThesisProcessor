b9ae6776991be1aaf9dc0562a596f815a5b642ac
dwhagar <david.hagar@gmail.com>
2019-02-19 09:04:05 -0500

    Fixed a bug in processing some of the XML data for nouns, ended up with a noun listed as "+n" rather than the actual word.

--- speaker/word.py
+++ speaker/word.py
@@ -15,13 +15,20 @@ class Word:
             self.word = word
         else:
             temp = word.split("|")
-            typ = temp[0]
-            word = temp[1]
+            if temp[1] == "+n":
+                typ = temp[0]
+                word = temp[2]
+            else:
+                typ = temp[0]
+                word = temp[1]
 
             if typ == "adj":
                 self.adj = True
             elif typ == "n":
                 self.noun = True
+
+            if word[-2:] == "+n":
+                word = word[:-2]
 
             self.word = word.split("&")[0]
             self.word = self.word.split("-")[0]
