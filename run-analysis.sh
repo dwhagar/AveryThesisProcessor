e3430c8836b1a9a9351a2212d9937a2dcb09ee3c
dwhagar <david.hagar@gmail.com>
2019-02-23 20:21:03 -0500

    Added a bash script to run the analysis on all the data and export the CSV files to the desktop.

---
+++ run-analysis.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./xml-process.py -r -d data/Lyon -o ~/Desktop/Lyon\ Stats
+./xml-process.py -r -d data/MTLN -o ~/Desktop/MTLN\ Stats
+./xml-process.py -r -d data/Palasis -o ~/Desktop/Palasis\ Stats
+./xml-process.py -r -d data -o ~/Desktop/All\ Stats
