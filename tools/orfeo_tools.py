53e6b60cae3ff1c7f57f70e33ca9dd663ab520c3
cyclops <david.hagar@gmail.com>
2021-06-24 22:13:19 -0400

    Began work on reading the actual sentence data.

--- tools/orfeo_tools.py
+++ tools/orfeo_tools.py
@@ -4,3 +4,10 @@ from .tools import read_text
 
 def read_sentences(file, speakers):
     """Reads sentence data from file and parses it with the speaker data."""
+    bulk_data = read_text(file)
+
+    sentences = []
+
+    for line in bulk_data:
+        if '\t' in line:
+            word_data = line.split('\t')
